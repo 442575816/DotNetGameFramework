58a795b24b05c7ae808a8cf92f9a4e4ca0b1f6f3
wangys <wangys@hariogame.com>
2020-11-06 16:51:15 +0800

    版本修改

---
+++ src/Buffer/ByteBufHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DotNetGameFramework.src.Buffer
+{
+    public class ByteBufHelper
+    {
+        public ByteBufHelper()
+        {
+        }
+    }
+}
---
+++ src/Buffer/Referenceable.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace DotNetGameFramework.src.Buffer
+{
+    public interface Referenceable
+    {
+    }
+}
